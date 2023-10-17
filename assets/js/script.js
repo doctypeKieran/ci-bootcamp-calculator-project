1bc2a3aa7b79b0462055f69a162eb11dba5825c5
Kieran Hillman <developedbykieran@gmail.com>
2023-10-17 10:19:55 +0000

    Add structure for buttons and use JS to render buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-console.log("SCRIPT DEPLOYED");
+document.addEventListener("DOMContentLoaded", () => {
+    const display = document.getElementById('display');
+    const buttons = ["7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "x", "C", "0", "=", "/"];
+
+    const calculatorButtons = document.getElementById('calculator-buttons');
+
+    // Create calculator buttons
+    buttons.forEach((button) => {
+        const btnElement = document.createElement("button");
+        btnElement.textContent = button;
+        btnElement.classList.add("btn", button === "=" || button === "C" ? "btn-danger" : "btn-primary");
+        btnElement.addEventListener("click", () => {
+            handleButtonClick(button)
+        })
+        calculatorButtons.appendChild(btnElement);
+    })
+
+    // Initialise the calculation variables
+    // Handle the button clicks
+});
